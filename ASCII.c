f052589e9ef2be82b402a8de94628452557a3a3e
250320100096-saswati <250320100096@cutm.ac.in>
2025-09-16 23:45:41 +0530

    Add files via upload

---
+++ ASCII.c
@@ -0,0 +1,9 @@
+//wap to find the ASCII value of a character.
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("enter a charecter:");
+    scanf("%c",&ch);
+    printf("the ASCII value of'%c'is %d\n",ch,ch);
+    return 0;
+}
